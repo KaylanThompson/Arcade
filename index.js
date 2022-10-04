70d16bfffcae8dfc906534339f5b105de86f8101
Kaylan Thompson <kaylanthompson@icloud.com>
2022-10-04 11:13:45 -0500

    made board and a function for selecting board size

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const table = document.createElement("table")
-const row = document.createElement("row")
+// MAKING THE BOARD * FUNCTION TO ADD A ROW * FUNCTION TO ADD A BIGGER BOARD THAT IS 6 x 6
+
+
 const section = document.getElementsByTagName("section")[0]
+const table = document.createElement("table")
+
 
 section.appendChild(table)
-table.appendChild(row)
+
+
+    function boardSize (numberOfCells) {
+        const row = document.createElement("tr")
+        for(let i = 0; i < numberOfCells; i++){
+            const cell = document.createElement("td")
+            row.appendChild(cell)
+        }
+        table.appendChild(row)
+    }
+
+
+
