b2a2cb48cead376ddb8bba19bd83c935a0a76fb4
Kaylan Thompson <kaylanthompson@icloud.com>
2022-10-04 16:09:33 -0500

    made players, started css borders for board

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-// MAKING THE BOARD * FUNCTION TO ADD A ROW * FUNCTION TO ADD A BIGGER BOARD THAT IS 6 x 6
 
-
+const body = document.getElementsByTagName("body")[0]
+console.log(body)
 const section = document.getElementsByTagName("section")[0]
 const table = document.createElement("table")
-
+const player1 = "x"
+const player2 = "o"
 
 section.appendChild(table)
 
+// FUNCTION TO PICK BOARD SIZE
 
     function boardSize (numberOfCells) {
         const row = document.createElement("tr")
@@ -16,6 +18,19 @@ section.appendChild(table)
         }
         table.appendChild(row)
     }
+
+boardSize(3)
+boardSize(3)
+boardSize(3)
+
+
+// const player1Name = createElement("input")
+// player1Name.classList.add("playerName")
+// body.player1Name(player1Name)
+
+
+
+
 
 
 
