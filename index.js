00d54452e557939ed3320611b9885aca6fd768a3
Kaylan Thompson <kaylanthompson@icloud.com>
2022-10-04 09:43:23 -0500

    just started

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hello")
+const table = document.createElement("table")
+const row = document.createElement("row")
+const section = document.getElementsByTagName("section")[0]
+
+section.appendChild(table)
+table.appendChild(row)
