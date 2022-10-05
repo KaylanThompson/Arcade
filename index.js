f6d0aacd51fa06ed8a7d01d0ec45c73992b2738f
Kaylan Thompson <kaylanthompson@icloud.com>
2022-10-05 08:22:38 -0500

    worked on trying to get the board to show up right

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
-const body = document.getElementsByTagName("body")[0]
-console.log(body)
 const section = document.getElementsByTagName("section")[0]
 const table = document.createElement("table")
+table.setAttribute("id", "board")
+
+const cell = document.getElementsByTagName("td")[0]
 const player1 = "x"
 const player2 = "o"
 
@@ -12,21 +13,32 @@ section.appendChild(table)
 
     function boardSize (numberOfCells) {
         const row = document.createElement("tr")
+        row.classList.add("row")
         for(let i = 0; i < numberOfCells; i++){
             const cell = document.createElement("td")
+            cell.classList.add("cell")
             row.appendChild(cell)
         }
         table.appendChild(row)
     }
 
+
 boardSize(3)
 boardSize(3)
 boardSize(3)
 
+const row1 = document.getElementsByTagName("tr")[0]
+row1.setAttribute("id", "row1")
+const row2 = document.getElementsByTagName("tr")[1]
 
-// const player1Name = createElement("input")
-// player1Name.classList.add("playerName")
-// body.player1Name(player1Name)
+row2.setAttribute("id", "row2")
+
+
+
+
+
+
+
 
 
 
